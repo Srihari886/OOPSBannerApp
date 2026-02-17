066d5a1f264d58b19509541c134fe00e33ab758e
Srihari886 <sriharis916@gmail.com>
2026-02-18 02:56:42 +0530

    Remove documentation comments from OOPSBannerApp
    Removed class-level documentation comments from OOPSBannerApp.

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,4 @@
-/**
- * OOPSBannerApp UC2 – Banner Display Application
- *
- * This class demonstrates the creation of a visual banner displaying the word "OOPS"
- * using ASCII art composed of asterisks (*) and spaces. The banner is printed to the
- * console using individual print statements for each line, creating a decorative
- * text effect. 
- *
- * @author Developer
- * @version 2.0 
- */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {

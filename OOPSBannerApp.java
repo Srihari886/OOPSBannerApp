47824fe72d34440fe3d93aa0c2921467a7e06f62
Srihari886 <sriharis916@gmail.com>
2026-02-18 02:59:24 +0530

    Refactored UC2 to use String.join() for UC3

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,16 @@
-/**
- * OOPSBannerApp UC2 – Banner Display Application
- *
- * This class demonstrates the creation of a visual banner displaying the word "OOPS"
- * using ASCII art composed of asterisks (*) and spaces. The banner is printed to the
- * console using individual print statements for each line, creating a decorative
- * text effect. 
- *
- * @author Developer
- * @version 2.0 
- */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        // Construct each line of the banner using string concatenation + 
-        // Each character is 9 characters wide and 7 lines high 
-        
-        System.out.println("   *** " + " " + "   *** " + " " + "****** " + " " + " ***** ");
-        System.out.println("  ** ** " + " " + "  ** ** " + " " + "** ** " + " " + "** ** ");
-        System.out.println(" ** ** " + " " + " ** ** " + " " + "** ** " + " " + "** ");
-        System.out.println(" ** ** " + " " + " ** ** " + " " + "****** " + " " + " ***** ");
-        System.out.println(" ** ** " + " " + " ** ** " + " " + "** " + " " + "      ** ");
-        System.out.println("  ** ** " + " " + "  ** ** " + " " + "** " + " " + "** ** ");
-        System.out.println("   *** " + " " + "   *** " + " " + "** " + " " + " ***** ");
+        // Use String.join(" ", ...) to join the parts of each line with spaces. [cite: 64]
+        // This is more efficient than the + operator for building patterns. [cite: 33]
+
+        System.out.println(String.join(" ", "   *** ", "   *** ", "****** ", " ***** "));
+        System.out.println(String.join(" ", "  ** ** ", "  ** ** ", "** ** ", " ** ** "));
+        System.out.println(String.join(" ", " ** ** ", " ** ** ", "** ** ", " ** "));
+        System.out.println(String.join(" ", " ** ** ", " ** ** ", "****** ", " ***** "));
+        System.out.println(String.join(" ", " ** ** ", " ** ** ", "** ", "      ** "));
+        System.out.println(String.join(" ", "  ** ** ", "  ** ** ", "** ", " ** ** "));
+        System.out.println(String.join(" ", "   *** ", "   *** ", "** ", " ***** "));
     }
 }

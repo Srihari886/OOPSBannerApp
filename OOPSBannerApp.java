93f93e089c05e083fed7282cbdb23c89db88a543
Srihari886 <sriharis916@gmail.com>
2026-02-18 02:44:43 +0530

    Coded UC2 to Render OOPS as Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,26 @@
 /**
- * OOPSBannerApp UC1 – OOPS Banner Display Application
+ * OOPSBannerApp UC2 – Banner Display Application
  *
- * This class demonstrates a simple Java application that displays the Object
- * Oriented Programming System OOPS acronym to the console.
+ * This class demonstrates the creation of a visual banner displaying the word "OOPS"
+ * using ASCII art composed of asterisks (*) and spaces. The banner is printed to the
+ * console using individual print statements for each line, creating a decorative
+ * text effect. 
  *
  * @author Developer
- * @version 1.0
+ * @version 2.0 
  */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // Construct each line of the banner using string concatenation + 
+        // Each character is 9 characters wide and 7 lines high 
+        
+        System.out.println("   *** " + " " + "   *** " + " " + "****** " + " " + " ***** ");
+        System.out.println("  ** ** " + " " + "  ** ** " + " " + "** ** " + " " + "** ** ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + "** ** " + " " + "** ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + "****** " + " " + " ***** ");
+        System.out.println(" ** ** " + " " + " ** ** " + " " + "** " + " " + "      ** ");
+        System.out.println("  ** ** " + " " + "  ** ** " + " " + "** " + " " + "** ** ");
+        System.out.println("   *** " + " " + "   *** " + " " + "** " + " " + " ***** ");
     }
 }
